3fc765db04cb02c54cf422bfdbcacc720926372b
Shekhar D <ai.implementer@gmail.com>
2026-03-07 11:27:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const API_BASE = "https://unsparse-sherman-nonfallaciously.ngrok-free.dev";
+
+const API = {
+  report: API_BASE + "/webhook/f020f006-eb25-4c31-9bbc-a5714df33af5",
+  health: API_BASE + "/health"
+};
