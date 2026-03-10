27568b0a2175b20ff1afa577f451b8c165063426
Shekhar D <ai.implementer@gmail.com>
2026-03-10 13:09:25 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const API = {
   report: atob("aHR0cHM6Ly91bnNwYXJzZS1zaGVybWFuLW5vbmZhbGxhY2lvdXNseS5uZ3Jvay1mcmVlLmRldi93ZWJob29rL2YwMjBmMDA2LWViMjUtNGMzMS05YmJjLWE1NzE0ZGYzM2FmNQ=="),
   health: atob("aHR0cHM6Ly91bnNwYXJzZS1zaGVybWFuLW5vbmZhbGxhY2lvdXNseS5uZ3Jvay1mcmVlLmRldi9oZWFsdGg="),
-  postlog: atob("aHR0cHM6Ly91bnNwYXJzZS1zaGVybWFuLW5vbmZhbGxhY2lvdXNseS5uZ3Jvay1mcmVlLmRldi93ZWJob29rL2U4MjEzMGEzLTk1NDctNDI2Zi04ZGRlLTQ2ZTM4NzM2NjVkOQ=="),
   service: atob("aHR0cHM6Ly9hcGkubWZhcGkuaW4vbWYvMTIwNTAz")
 };
